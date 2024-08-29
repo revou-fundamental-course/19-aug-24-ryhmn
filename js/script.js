4c40ebc501b4983417c106f396f8107e0783e6ce
Dary Rahman <dary.rahman30@gmail.com>
2024-08-29 23:50:20 +0700

    fix: improve the responsivenes display for phone and tablet and add some features - Added carousel or slider effect to the banner - Added validation to the form, when one of those fields is empty then the allert will be displayed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-window.onload = function () {
+// Inout name prompt
+function inputNamePrompt() {
   var name = prompt("Please enter your name:");
   if (name) {
     document.querySelector(".name").textContent = name;
@@ -8,12 +9,44 @@ window.onload = function () {
   }
 }
 
+window.onload = inputNamePrompt;
+
+// Form validation
 document.querySelector("form").addEventListener('submit', function (event) {
   event.preventDefault();
 
-  document.getElementById("display-current-time").textContent = new Date().toLocaleString();
-  document.getElementById("display-name").textContent = document.querySelector('[name="name"]').value;
-  document.getElementById("display-born-date").textContent = document.querySelector('[name="born-date"]').value;
-  document.getElementById("display-gender").textContent = document.querySelector('[name="gender"]').value;
-  document.getElementById("display-message").textContent = document.querySelector('[name="message"]').value;
-}) 
+  let getName = document.querySelector('[name="name"]').value;
+  let getBornDate = document.querySelector('[name="born-date"]').value;
+  let getGender = document.querySelector('[name="gender"]').value;
+  let getMessage = document.querySelector('[name="message"]').value;
+
+  if (getName == "" || getBornDate == "" || getGender == "" || getMessage == "") {
+    alert("Please fill all your identities!");
+  } else {
+    document.getElementById("display-current-time").textContent = new Date().toLocaleString();
+    document.getElementById("display-name").textContent = getName;
+    document.getElementById("display-born-date").textContent = getBornDate;
+    document.getElementById("display-gender").textContent = getGender;
+    document.getElementById("display-message").textContent = getMessage;
+  }
+
+})
+
+// Banner images slider
+var images = [
+  'url("https://picsum.photos/id/101/1366/576?grayscale")',
+  'url("https://picsum.photos/id/58/1366/576?grayscale")',
+  'url("https://picsum.photos/id/144/1366/576?grayscale")'
+];
+
+var currentIndex = 0;
+
+function changeBackground() {
+  var slider = document.getElementById("banner");
+  slider.style.backgroundImage = images[currentIndex];
+  currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeBackground, 3000);
+
+window.onload = changeBackground;
